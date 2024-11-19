867bed0c126943891fd77fa5762616c246d4ff37
joshrodriguescae <125597302+joshrodriguescae@users.noreply.github.com>
2024-11-19 18:27:33 -0300

    Add files via upload

---
+++ carros_web/config.php
@@ -0,0 +1,14 @@
+<?php
+define('HOST','localhost');
+define('USER','root');
+define('pass','');
+define('DBBASE','carros');
+
+ $conn = new mysqli(HOST,USER,PASS,DBBASE);
+
+   if ($conn->connect_error);{
+    echo "não conectado.$conn->connect_error";
+   }else{
+    # echo 'banco conectado';
+    
+   }
